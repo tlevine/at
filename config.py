b46e017d6ddf508ccf482fe45d2c069947c5a757
Thomas Levine <_@thomaslevine.com>
2014-07-25 11:53:00 -0400

    rewrite config as argparse

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
-debug = True
+import argparse
 
-port = 8080
-
-db = './at.db'
-cap_file = './dhcp-cap'
-lease_file = './leases'
-lease_offset = 60 * 20
-timeout = 3000
-
-wiki_url = 'http://hackerspace.pl/wiki/doku.php?id=people:%(login)s:start'
+parser = argparse.ArgumentParser()
+parser.add_argument('--debug', action = 'store_false')
+parser.add_argument('--port', default = 8080, type = int)
+parser.add_argument('--db', default = './at.db')
+parser.add_argument('--cap-file', default = './dhcp-cap')
+parser.add_argument('--lease-file', default = './leases')
+parser.add_argument('--lease-offset', help = 'Lease offset, in seconds',
+    type = lambda x: int(x) * 60, default = 20)
+parser.add_argument('--timeout', default = 3000, type = int)
+parser.add_argument('--wiki-url', default = 'http://hackerspace.pl/wiki/doku.php?id=people:%(login)s:start')
+parser.add_argument('--secret-key', default = 'adaba')
 
 claimable_prefix = '' #'192.168.1.'
 claimable_exclude = [
 #    '127.0.0.1',
 ]
-
-secret_key = 'adaba'
