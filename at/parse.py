73c0b4ba7b335c15841f0a3e09aa6c39b381666d
Thomas Levine <_@thomaslevine.com>
2014-07-25 13:41:48 -0400

    semicolon and stuff

--- at/parse.py
+++ at/parse.py
@@ -6,14 +6,14 @@ from iscpy import ParseISCString
 def lease_file(fp, lease = False):
     leases = ParseISCString(fp.read())
     for l in leases:
-        lease = l.values()[0]
+        ip = l.keys()[0].split(' ')[1]
 
-        ip = lease.keys()[0].split(' ')[1]
+        lease = l.values()[0]
 
         hwaddr = lease['hardware'].split(' ')[1]
 
-        _starts = datetime.strptime(lease['starts'][2:], '%Y/%m/%d %H:%M:%S;')
-        atime = mktime(dt.utctimetuple())
+        _starts = datetime.strptime(lease['starts'][2:], '%Y/%m/%d %H:%M:%S')
+        atime = mktime(_starts.utctimetuple())
 
         if 'client-hostname' in lease:
             name = lease['client-hostname'][1][1:-2]
