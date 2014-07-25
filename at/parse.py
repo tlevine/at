c69e267eebf161cf06f6f2b57adb3b44e155ad52
Thomas Levine <_@thomaslevine.com>
2014-07-25 13:33:26 -0400

    names

--- at/parse.py
+++ at/parse.py
@@ -1,26 +1,22 @@
 from datetime import datetime
 from time import mktime
 
+from iscpy import ParseISCString
+
 def lease_file(fp, lease = False):
-    for line in fp:
-        line = line.split('#')[0]
-        cmd = line.strip().split()
-        if not cmd:
-            continue
-        if lease:
-            field = cmd[0]
-            if(field == 'starts'):
-                dt = datetime.strptime(' '.join(cmd[2:]), '%Y/%m/%d %H:%M:%S;')
-                atime = mktime(dt.utctimetuple())
-            if(field == 'client-hostname'):
-                name = cmd[1][1:-2]
-            if(field == 'hardware'):
-                hwaddr = cmd[2][:-1]
-            if(field.startswith('}')):
-                lease = False
-                if hwaddr:
-                    yield (hwaddr, atime, ip, name)
-        elif cmd[0] == 'lease':
-            ip = cmd[1]
-            name, hwaddr, atime = [None] * 3
-            lease = True
+    leases = ParseISCString(fp.read())
+    for l in leases:
+        lease = l.values()[0]
+
+        ip = lease.keys()[0].split(' ')[1]
+
+        hwaddr = lease['hardware'].split(' ')[1]
+
+        _starts = datetime.strptime(lease['starts'][2:], '%Y/%m/%d %H:%M:%S;')
+        atime = mktime(dt.utctimetuple())
+
+        if 'client-hostname' in lease:
+            name = lease['client-hostname'][1][1:-2]
+        else:
+            name = None
+        yield (hwaddr, atime, ip, name)
--- at/test/test_parse.py
+++ at/test/test_parse.py
@@ -6,8 +6,7 @@ import nose.tools as n
 import at.parse as parse
 
 def test_lease_file():
-    with open(os.path.join('at','test','fixtures','lease-file')) as fp:
+    with open(os.path.join('at','test','fixtures','leases')) as fp:
         observed = list(parse.lease_file(fp))
-    with open(os.path.join('at','test','fixtures','lease-file.json')) as fp:
-        expected = json.load(fp)
+    from at.test.fixtures.leases import leases as expected
     n.assert_list_equal(observed, expected)
