385bfd88e5a76d7b77c8d10b19e5715e3df48823
Thomas Levine <_@thomaslevine.com>
2014-07-25 12:30:58 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(name='at-hackerspace',
+      author='Tomek Dubrownik',
+      author_email='<t.dubrownik@gmail.com>',
+      maintainer='Thomas Levine',
+      maintainer_email='_@thomaslevine.com',
+      description='Determine who\'s in the hackerspace based on DHCP leases.',
+      url='https://github.com/tlevine/at',
+      packages=['at-hackerspace'],
+      install_requires = [
+          'flask',
+          'werkzeug',
+          'wsgiref',
+          'pesto',
+          'pcapy',
+      ],
+      scripts = [
+          'at-hackerspace',
+      ],
+      tests_require = ['nose'],
+      version='0.0.3',
+      license='AGPL',
+      classifiers=[
+          'Programming Language :: Python :: 2.7',
+      ],
+)
