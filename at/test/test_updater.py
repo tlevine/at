7249803bd7aac6940fd97f05ccd1cedbc94f367a
Thomas Levine <_@thomaslevine.com>
2014-07-26 04:24:15 -0400

    another test

--- at/test/test_updater.py
+++ at/test/test_updater.py
@@ -1,6 +1,7 @@
 import nose.tools as n
 
 import at.updater as u
+
 def test_purge_stale():
     timeout = 60
     active_devices = {
@@ -10,3 +11,12 @@ def test_purge_stale():
     expected = {'addr1': active_devices['addr1']}
     observed = u.purge_stale(timeout, active_devices, now = 1406362750.570188)
     n.assert_dict_equal(observed, expected)
+
+def test_get_device():
+    active_devices = {
+        'addr1': (1406362740.570188, '192.168.1.2', 'tom'),
+        'addr2': (1006362740.0, '192.168.1.82', 'william'),
+    }
+    n.assert_tuple_equal(u.get_device(active_devices, '192.168.1.82'), ('addr2', 'william'))
+    n.assert_tuple_equal(u.get_device(active_devices, '192.168.4.10'), (None, None))
+
