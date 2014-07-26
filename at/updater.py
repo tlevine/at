59940447f2eba1e8e073adeb40d3123e70688487
Thomas Levine <_@thomaslevine.com>
2014-07-26 02:53:17 -0400

    functions might work now

--- at/updater.py
+++ at/updater.py
@@ -9,57 +9,49 @@ import parse
 
 logger = getLogger('at')
 
-active_devices = {}
-timeout = # config.timeout
+def purge_stale(timeout, active_devices):
+    active = dict(active_devices)
+    now = time()
+    for addr, (atime, ip, name) in active.items():
+        if now - atime > timeout:
+            del active[addr]
 
-    def purge_stale(active_devices):
-        active = dict(active_devices)
-        now = time()
-        for addr, (atime, ip, name) in active.items():
-            if now - atime > timeout:
-                del active[addr]
+def get_device(active_devices, ip):
+    for hwaddr, (atime, dip, name) in \
+        active_devices.iteritems():
+        if ip == dip:
+            return hwaddr, name
+    return None, None
 
-    def get_device(active_devices, ip):
-        for hwaddr, (atime, dip, name) in \
-            active_devices.iteritems():
-            if ip == dip:
-                return hwaddr, name
-        return None, None
+def update(lease_offset, active_devices, hwaddr, atime = None, ip = None, name = None):
+    _active_devices = dict(active_devices)
+    if atime:
+        atime -= lease_offset
+    else:
+        atime = time() 
+    _active_devices[hwaddr] = (atime, ip, name)
+    logger.info('updated %s with atime %s and ip %s',
+        hwaddr, util.strfts(atime), ip)
+    return _active_devices
 
-    def update(active_devices, hwaddr, atime = None, ip = None, name = None):
-        _active_devices = dict(active_devices)
-        if atime:
-            atime -= self.lease_offset
-        else:
-            atime = time() 
-        _active_devices[hwaddr] = (atime, ip, name)
-        logger.info('updated %s with atime %s and ip %s',
-            hwaddr, util.strfts(atime), ip)
-        return _active_devices
 
-class MtimeUpdater(Updater):
-    def __init__(self, lease_file, *a, **kw):
-        self.lease_file = lease_file
-        self.last_modified = 0
-        Updater.__init__(self, *a, **kw)
-    def file_changed(self, f):
-        pass
-    def run(self):
-        while True:
-            if not os.path.isfile(self.lease_file):
-                logger.error('Lease file %s does not exist.' % self.lease_file)
-                break
-            try:
-                mtime = os.stat(self.lease_file).st_mtime
-                if mtime > self.last_modified:
-                    logger.info('Lease file changed, updating')
-                    with open(self.lease_file, 'r') as f:
-                        for hwaddr, atime, ip, name in parse.lease_file(f):
-                            self.update(hwaddr, atime, ip, name)
-                self.last_modified = mtime
-                sleep(3.0)
-            except Exception as e:
-                logger.error('Updater got an exception:\n' + \
-                    traceback.format_exc(e))
-                sleep(10.0)
-
+def watch(lease_file, last_modified = 0, active_devices = {}):
+    while True:
+        if not os.path.isfile(lease_file):
+            logger.error('Lease file %s does not exist.' % lease_file)
+            break
+        try:
+            mtime = os.stat(lease_file).st_mtime
+            if mtime > last_modified:
+                logger.info('Lease file changed, updating')
+                with open(lease_file, 'r') as f:
+                    for hwaddr, atime, ip, name in parse.lease_file(f):
+                        update(active_devices, hwaddr, atime, ip, name)
+            last_modified = mtime
+            sleep(3.0)
+        except KeyboardInterrupt:
+            break
+        except Exception as e:
+            logger.error('Updater got an exception:\n' + \
+                traceback.format_exc(e))
+            sleep(10.0)
